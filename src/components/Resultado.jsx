d47ad736cce61baa7bec22acda7d6a3e3a72a7ff
Diego <diegoemg996@gmail.com>
2022-03-17 14:18:55 -0600

    docs

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -4,7 +4,6 @@ import { ResultInfo } from './ResultInfo'
 export const Resultado = ({imc}) => {
   return (
     <div className='my-2'>
-
         
         {
             imc > 0 && <h4>Tu IMC es: <span>{imc}</span> </h4>
